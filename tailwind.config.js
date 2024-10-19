b97c07d735723bf38df297d368bbf4456b88319b
Scott <squiggs.rafael@gmail.com>
2024-10-19 15:22:59 -0300

    task: configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{gjs,gts,hbs,html,js,ts}',
+    // './node_modules/snapp-ui-beta/app/**/*.{gjs,gts,hbs,html,js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
